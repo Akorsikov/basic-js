a942fefcccc6fd9db13f5fb85947e51ab55f8d73
Akorsikov <akors@mail.ru>
2022-05-02 03:16:22 +0300

    feat: recursive & simle & transform

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -13,12 +13,22 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  calculateDepth(arr) {
+    let countOut = 0;
+    for (let i = 0; i <= arr.length; i++) {
+      let count = 1;
+      if (Array.isArray(arr[i])) {
+        //count++;
+        count += this.calculateDepth(arr[i]);
+      }
+      if (count > countOut) countOut = count;
+    }
+    return countOut;
   }
 }
 
+//const depthCalc = new DepthCalculator();
+//depthCalc.calculateDepth([1, [8, [[]]], 2, 3, [8, []], 4, 5, []]);
 module.exports = {
   DepthCalculator
 };
